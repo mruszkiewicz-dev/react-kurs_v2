c1f841106fef828a097e4ba0aed97a6726ee7512
monkeymr014 <monkeymr014@gmail.com>
2022-12-06 14:24:47 +0100

    up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App() {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
