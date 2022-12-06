27eaa5961341ea66e22d3e6ddd774a940a8914ed
monkeymr014 <monkeymr014@gmail.com>
2022-12-06 14:06:37 +0100

    lesson 3.1

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+funct App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
