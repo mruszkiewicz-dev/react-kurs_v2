9550fd0cc98fc88a1c7c6efc35badf043a152f43
monkeymr014 <monkeymr014@gmail.com>
2022-12-07 19:43:56 +0100

    lesson 4.3

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,10 @@
+// .storybook/preview.js
+
+import { themes } from '@storybook/theming';
+
+// or global addParameters
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
+  docs: {
+    theme: themes.dark,
   },
 };
