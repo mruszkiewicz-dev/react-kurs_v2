6c9d4036cbac226d06649b0b09fa02be889d3d08
monkeymr014 <monkeymr014@gmail.com>
2022-12-12 11:11:43 +0100

    lesson 5.2

--- src/components/atoms/Button/Buttonicon/Buttonicon.js
+++ src/components/atoms/Button/Buttonicon/Buttonicon.js
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
-const Buttonicon = styled.input``;
+const Buttonicon = styled.button`
+  width: 67px;
+  height: 67px;
+  border-radius: 20px;
+  background-image: url(${({ icon }) => icon});
+  background-repeat: no-repeat;
+  background-position: 50%;
+  border: none;
+`;
 
 export default Buttonicon;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -5,6 +5,8 @@ import Heading from 'components/atoms/Heading/Heading';
 import Paragraph from 'components/atoms/Paragraph/Paragraph';
 import GlobalStyle from 'theme/GlobalStyle';
 import Input from 'components/atoms/Input/Input';
+import Buttonicon from 'components/atoms/Button/Buttonicon/Buttonicon';
+import bulbIcon from 'assets/icons/bulb.svg';
 import { theme } from 'theme/mainTheme';
 
 const Root = () => (
@@ -14,6 +16,7 @@ const Root = () => (
       <>
         <Heading big>Big</Heading>
         <Heading>Normall</Heading>
+        <Buttonicon icon={bulbIcon} />
         <Paragraph>
           Miles Dewey Davis III (May 26, 1926 – September 28, 1991) was an American jazz trumpeter,
           bandleader, and composer.
